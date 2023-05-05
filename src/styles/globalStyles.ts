4e57a349fed1a4dca86368d5c2cf3a3fc970e9c8
HenoL-rgb <sasha.diretic@gmail.com>
2023-05-05 21:52:44 +0300

    project init

---
+++ src/styles/globalStyles.ts
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    :root {
+        font-family: 'IBM Plex Sans', system-ui, Avenir, Helvetica, Arial, sans-serif;
+        font-weight: 400;
+        font-synthesis: none;
+        text-rendering: optimizeLegibility;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        -webkit-text-size-adjust: 100%;
+    }
+    body {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+`;
+
+export default GlobalStyle;
